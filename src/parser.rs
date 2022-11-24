6196a27bb1fc0a9d1fa60d14446a7c43032987d8
o-tsaruk <oleksandr.v.tsaruk@gmail.com>
2022-11-24 02:35:35 +0200

    fix parse_struct

--- src/parser.rs
+++ src/parser.rs
@@ -939,7 +939,7 @@ impl<'a> Parser<'a> {
         let regex = Regex::new(
             r#"(?x)
                 (?P<comment1>(\n\s*//.*)*|(\n\s*/\*(.*\n)*?.*\*/\s*))?
-                (?P<field>\n\s*[A-Za-z0-9=>()_]+\s+[A-Za-z0-9_]+\s*;)
+                (?P<field>\n\s*[A-Za-z0-9=>()\[\]_]+\s+[A-Za-z0-9_]+\s*;)
                 (?P<comment2>(.*//.*)|(.*/\*(.*\n)*?.*\*/))?"#,
         )
         .unwrap();
