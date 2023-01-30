148e8b0051a8427d5307e6452b463f0a1be7ac3c
Dominik | Supercolony <43150707+coreggon11@users.noreply.github.com>
2023-01-31 00:38:51 +0100

    generate examples

---
+++ examples/generated/contracts/stable_swap/lib.rs
@@ -0,0 +1,69 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+#![feature(min_specialization)]
+
+// Generated with Sol2Ink v2.0.0-beta
+// https://github.com/727-Ventures/sol2ink
+
+#[openbrush::contract]
+pub mod stable_swap {
+    use generated::*;
+    use ink_lang::codegen::{
+        EmitEvent,
+        Env,
+    };
+    use ink_prelude::vec::*;
+    use ink_storage::traits::SpreadAllocate;
+    use openbrush::{
+        storage::Mapping,
+        traits::{
+            AccountId,
+            AccountIdExt,
+            Storage,
+            String,
+            ZERO_ADDRESS,
+        },
+    };
+    use scale::{
+        Decode,
+        Encode,
+    };
+
+    /// Number of tokens
+    pub const N: u128 = 3;
+    /// Amplification coefficient multiplied by N^(N - 1)
+    /// Higher value makes the curve more flat
+    /// Lower value makes the curve more like constant product AMM
+    pub const A: u128 = 1000 * (N.pow((N - 1)));
+    /// 0.03%
+    pub const SWAP_FEE: u128 = 300;
+    /// Liquidity fee is derived from 2 constraints
+    /// 1. Fee is 0 for adding / removing liquidity that results in a balanced pool
+    /// 2. Swapping in a balanced pool is like adding and then removing liquidity
+    ///    from a balanced pool
+    /// swap fee = add liquidity fee + remove liquidity fee
+    pub const LIQUIDITY_FEE: u128 = (SWAP_FEE * N) / (4 * (N - 1));
+    pub const FEE_DENOMINATOR: u128 = 1000000;
+    /// 1 share = 1e18, 18 decimals
+    pub const DECIMALS: u128 = 18;
+
+    #[ink(storage)]
+    #[derive(Default, SpreadAllocate, Storage)]
+    pub struct StableSwapContract {
+        #[storage_field]
+        data: impls::Data,
+    }
+
+    impl StableSwap for StableSwapContract {}
+
+    impl stable_swap::Internal for StableSwapContract {}
+
+    impl StableSwapContract {
+        #[ink(constructor)]
+        pub fn new() -> Self {
+            ink_lang::codegen::initialize_contract(|instance: &mut Self| {
+                self.multipliers = [1, 1000000000000, 1000000000000];
+            })
+        }
+
+    }
+}
---
+++ examples/generated/src/traits/array_contract.rs
@@ -0,0 +1,59 @@
+// Generated with Sol2Ink v2.0.0-beta
+// https://github.com/727-Ventures/sol2ink
+
+use ink_prelude::vec::*;
+use openbrush::{
+    storage::Mapping,
+    traits::{
+        AccountId,
+        AccountIdExt,
+        String,
+        ZERO_ADDRESS,
+    },
+};
+use scale::{
+    Decode,
+    Encode,
+};
+
+#[derive(Debug, Encode, Decode, PartialEq, Eq)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+pub enum Error {
+    Custom(String),
+}
+
+
+#[derive(Default, Encode, Decode)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+pub struct TestStruct {
+    struct_mapping: Mapping<u128, u128>,
+    struct_f_array: [u8; 32],
+    struct_d_array: Vec<u128>,
+}
+
+#[derive(Default, Encode, Decode)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+pub struct NestedTestStruct {
+    test_struct: TestStruct,
+    uint_field: u8,
+}
+
+
+#[openbrush::wrapper]
+pub type ArrayContractRef = dyn ArrayContract;
+
+#[openbrush::trait_definition]
+pub trait ArrayContract {
+    #[ink(message)]
+    fn storage_mapping(&self) -> Mapping<u128, u128>;
+
+    #[ink(message)]
+    fn storage_f_array(&self) -> Vec<u128>;
+
+    #[ink(message)]
+    fn storage_d_array(&self) -> Vec<u128>;
+
+    #[ink(message)]
+    fn storage_d_struct_array(&self) -> Vec<NestedTestStruct>;
+
+}
